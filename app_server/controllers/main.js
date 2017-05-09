dacc67969525d6369b5766d2655ab17bfb0b2453
Buiteman <stijn.buiteman@sogeti.com>
2017-05-09 13:36:19 +0200

    merge fix

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,5 +1,13 @@
 var path = require('path');
+var doa = require('../modules/dao.js')
 
 module.exports.index = function(err, res) {
     res.sendFile(path.resolve('app_server/views/index.html'));
+};
+
+module.exports.test = function(err, res) {
+   doa.test(function (data) {
+       console.log(data);
+       res.send(data);
+   });
 };
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -4,5 +4,6 @@ var ctrlmain = require('../controllers/main');
 
 /* GET home page. */
 router.get('/',ctrlmain.index);
+router.get('/test',ctrlmain.test);
 
 module.exports = router;
