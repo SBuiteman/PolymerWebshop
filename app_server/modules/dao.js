eefa2bd53195d8700d76a59d7b178864f6b5e09e
Engelenburg <rick.van.engelenburg@sogeti.com>
2017-05-17 10:51:04 +0200

    added categorie query stuff

--- app_server/modules/dao.js
+++ app_server/modules/dao.js
@@ -5,9 +5,23 @@ var ObjectId = require('mongodb').ObjectID;
 var async = require('async');
 
 module.exports.findAllProducts = function (cb) {
-    db.products.find({}, function (err, res) {
-        if (res.length > 0) {
-            cb(res);
+    db.products.find({}, function (err, products) {
+        if (products.length > 0) {
+
+            async.forEach(products, function (product, callback) {
+                db.categories.find({ "_id": ObjectId(product.categorie) }, function (err, categorieName) {
+                    console.log("cat = " + categorieName[0].categorie);
+                    product.categorie = categorieName[0].categorie;
+                    callback();
+                });
+
+            }, function () {
+                console.log(products);
+                //socket.emit('deckReturn', deck);
+                cb(products);
+            });
+
+
         }
     });
 }
@@ -19,3 +33,11 @@ module.exports.findAllCategories = function (cb) {
         }
     });
 };
+
+module.exports.getCardRace = function (raceId, cb) {
+    db.races.find({ _id: raceId }, function (err, res) {
+        if (res.length > 0) {
+            cb(res);
+        }
+    });
+}
